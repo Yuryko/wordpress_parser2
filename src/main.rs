78935ce1f68391c20afe3b1f46683920b5e7e999
Kozlov Yury <kye@cryptosoft.ru>
2025-03-17 11:52:14 +0300

    [wordpress_parser2] Добавлено удаление лишних постов.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use zip::ZipArchive;
 use encoding::{Encoding, DecoderTrap};
 use encoding::all::WINDOWS_437;
 use regex::Regex;
+use std::fs;
+use std::io::{self, BufRead};
+use walkdir::WalkDir;
+
 
 pub fn unzip() -> io::Result<()> {
     // Открываем файл file.zip
@@ -37,9 +41,43 @@ pub fn unzip() -> io::Result<()> {
     Ok(())
 }
 
-pub fn delete() io::Result<()> {
-    let regex = Regex::new(r"\d{3}").unwrap();
-    let a: &str = "123bbasfsdf23asd2021-06-17";
+
+// удалим все лишние посты
+
+fn del_post() -> io::Result<()> {
+    let dir_path = "."; // путь к каталогу, который нужно обработать
+
+    for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
+        if entry.file_type().is_file() {
+            let file_path = entry.path();
+            if !contains_string(file_path, "Для внешней публикации")? {
+                println!("Удаление файла: {:?}", file_path);
+                fs::remove_file(file_path)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn contains_string(file_path: &std::path::Path, search_string: &str) -> io::Result<bool> {
+    let file = fs::File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.contains(search_string) {
+            return Ok(true);
+        }
+    }
+
+    Ok(false)
+}
+
+// надо переделать все регулярки из питона
+//pub fn delete() io::Result<()> {
+//    let regex = Regex::new(r"\d{3}").unwrap();
+//    let a: &str = "123bbasfsdf23asd2021-06-17";
     //files = os.listdir(root_dir)
     //for each_file in files:
     //    full_path = "%s/%s" % (root_dir, each_file)
@@ -47,11 +85,12 @@ pub fn delete() io::Result<()> {
     //    if not any(word in each_file_content for word in words):
     //       os.unlink(full_path)
 
-}
+//}
 
 fn main(){
     use::unzip;
     unzip();
+    del_post();
 
 
 }
